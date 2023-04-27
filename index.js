5110deccbf7d1c6926aaecbf4c5a6b26c8b7a300
sammyrey6 <121529677+sammyrey6@users.noreply.github.com>
2023-04-26 18:35:51 -0700

    fixed inquirer

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-const inquirer = require ('inquirer')
-const fs = require('fs')
-const {circle, square, triangle} = require('./shapes/Shapes')
+const inquirer = require ('inquirer');
+const fs = require('fs');
+const {Circle, Square, Triangle} = require('./library/shapes');
+
+class Svg{
+    constructor(){
+        this.textElement=""
+        this.shapeElement=''
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http:www.w3.org/2000/svg" width='300' height="200">`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill=${color}`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}
+inquirer
+.prompt([
+
+    {
+        type :"input",
+        name : 'text',
+        message : "TEXT: Enter 3=<x Characters"
+    },
+    {
+        type :"input",
+        name : 'text-color',
+        message : "TEXT COLOR: Enter a color keyword (or a hexadecimal number)"
+    },
+    {
+        type :"input",
+        name : 'shape',
+        message : "Shape Color: Enter a color keyword (or a hexadecimal number)"
+    },
+    {
+        type :"list",
+        name : 'pixel-image',
+        message : "Choose your Image:",
+        choices: ['Circle', 'Square', 'Triangle']
+    },
+    
+])
+.then((answers)=> {
+    console.log(answers);
+    fs.writeFile('new.svg', Svg(answers), (err)=>{
+
+    });
+
+});
+
+
+)
